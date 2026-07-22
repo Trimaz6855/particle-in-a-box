73cd669fa00d34160de8dd6e5f694ed22ec0037c
Harry Tilling <harrytilling0407@gmail.com>
2026-07-22 18:06:45 +0100

    Started working on implementing the 1D probabilty density and amplitude graphing.
    Updated 2D pd and amplitude to use the value of lx and ly when calculating the array of x and y values to be used in calculation.

--- code/plot2Dim.py
+++ code/plot2Dim.py
@@ -10,8 +10,8 @@ def plot_2d_prob_density(l_x_val, l_y_val, n_x_val, n_y_val):
     plt.figtext(0.01, 0.5, f"L_x: {l_x_val}\nL_y: {l_y_val}\nn_x: {n_x_val}\nn_y: {n_y_val}")
     l_x, l_y, n_x, n_y, x, y = sp.symbols("l_x, l_y, n_x, n_y, x, y")
     equation = sp.sympify(sp.sqrt(4/(l_x * l_y)) * sp.sin((np.pi * n_x * x) / (l_x)) * sp.sin((np.pi * n_y * y) / (l_y)))
-    x_vals = np.arange(0, 1, 0.001)
-    y_vals = np.arange(0, 1, 0.001)
+    x_vals = np.arange(0, l_x_val, 0.001)
+    y_vals = np.arange(0, l_y_val, 0.001)
     x_vals, y_vals = np.meshgrid(x_vals, y_vals)
     prob_density = sp.lambdify([l_x, l_y, n_x, n_y, x, y], equation ** 2, "numpy")
     prob_density_values = prob_density(l_x_val, l_y_val, n_x_val, n_y_val, x_vals, y_vals)
@@ -21,21 +21,18 @@ def plot_2d_prob_density(l_x_val, l_y_val, n_x_val, n_y_val):
 
 def plot_2d_wave_function(l_x_val, l_y_val, n_x_val, n_y_val):
     fig = plt.figure(figsize=plt.figaspect(0.5))        
-    plt.figtext(0.01, 0.5, f"L_x: {l_x_val}\nL_y: {l_y_val}\nn_x: {n_x_val}\nn_y: {n_y_val}")
     fig.suptitle("Graph of the wave function for a 2D particle in a box system")
+    plt.figtext(0.01, 0.5, f"L_x: {l_x_val}\nL_y: {l_y_val}\nn_x: {n_x_val}\nn_y: {n_y_val}")
     l_x, l_y, n_x, n_y, x, y = sp.symbols("l_x, l_y, n_x, n_y, x, y")
     equation = sp.sympify(sp.sqrt(4/(l_x * l_y)) * sp.sin((np.pi * n_x * x) / (l_x)) * sp.sin((np.pi * n_y * y) / (l_y)))
-    x_vals = np.arange(0, 1, 0.001)
-    y_vals = np.arange(0, 1, 0.001)
+    x_vals = np.arange(0, l_x_val, 0.001)
+    y_vals = np.arange(0, l_y_val, 0.001)
     x_vals, y_vals = np.meshgrid(x_vals, y_vals)
     wave_function = sp.lambdify([l_x, l_y, n_x, n_y, x, y], equation, "numpy")
     wave_function_values = wave_function(l_x_val, l_y_val, n_x_val, n_y_val, x_vals, y_vals)
     ax = fig.add_subplot(1,1,1, projection="3d")
     ax.plot_surface(x_vals, y_vals, wave_function_values, cmap=cm.inferno)
     plt.show()
-    
-
-
 
 if __name__ == "__main__":
     plot_2d_wave_function(l_x_val = 1, l_y_val = 1, n_x_val = 4, n_y_val = 4)
