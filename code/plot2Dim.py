7d2156721a95ed1e9cb178f76916c4acc62782a8
Harry Tilling <harrytilling0407@gmail.com>
2026-07-22 19:10:50 +0100

    Updated 1D and 2D plot python files.

--- code/plot2Dim.py
+++ code/plot2Dim.py
@@ -7,7 +7,7 @@ from matplotlib import cm
 def plot_2d_prob_density(l_x_val, l_y_val, n_x_val, n_y_val):
     fig = plt.figure(figsize=plt.figaspect(0.5))
     fig.suptitle("Graph of the probabilty density function for a 2D particle in a box system")
-    plt.figtext(0.01, 0.5, f"L_x: {l_x_val}\nL_y: {l_y_val}\nn_x: {n_x_val}\nn_y: {n_y_val}")
+    plt.figtext(0.01, 0.7, f"L_x: {l_x_val}\nL_y: {l_y_val}\nn_x: {n_x_val}\nn_y: {n_y_val}")
     l_x, l_y, n_x, n_y, x, y = sp.symbols("l_x, l_y, n_x, n_y, x, y")
     equation = sp.sympify(sp.sqrt(4/(l_x * l_y)) * sp.sin((np.pi * n_x * x) / (l_x)) * sp.sin((np.pi * n_y * y) / (l_y)))
     x_vals = np.arange(0, l_x_val, 0.001)
@@ -17,12 +17,15 @@ def plot_2d_prob_density(l_x_val, l_y_val, n_x_val, n_y_val):
     prob_density_values = prob_density(l_x_val, l_y_val, n_x_val, n_y_val, x_vals, y_vals)
     ax = fig.add_subplot(1, 1, 1, projection="3d")
     ax.plot_surface(x_vals, y_vals, prob_density_values, cmap=cm.inferno)
+    ax.set_xlabel("x Position")
+    ax.set_ylabel("y Position")
+    ax.set_zlabel("Probability Density")
     plt.show()
 
 def plot_2d_wave_function(l_x_val, l_y_val, n_x_val, n_y_val):
     fig = plt.figure(figsize=plt.figaspect(0.5))        
     fig.suptitle("Graph of the wave function for a 2D particle in a box system")
-    plt.figtext(0.01, 0.5, f"L_x: {l_x_val}\nL_y: {l_y_val}\nn_x: {n_x_val}\nn_y: {n_y_val}")
+    plt.figtext(0.01, 0.7, f"L_x: {l_x_val}\nL_y: {l_y_val}\nn_x: {n_x_val}\nn_y: {n_y_val}")
     l_x, l_y, n_x, n_y, x, y = sp.symbols("l_x, l_y, n_x, n_y, x, y")
     equation = sp.sympify(sp.sqrt(4/(l_x * l_y)) * sp.sin((np.pi * n_x * x) / (l_x)) * sp.sin((np.pi * n_y * y) / (l_y)))
     x_vals = np.arange(0, l_x_val, 0.001)
@@ -32,6 +35,9 @@ def plot_2d_wave_function(l_x_val, l_y_val, n_x_val, n_y_val):
     wave_function_values = wave_function(l_x_val, l_y_val, n_x_val, n_y_val, x_vals, y_vals)
     ax = fig.add_subplot(1,1,1, projection="3d")
     ax.plot_surface(x_vals, y_vals, wave_function_values, cmap=cm.inferno)
+    ax.set_xlabel("x Position")
+    ax.set_ylabel("y Position")
+    ax.set_zlabel("Amplitude")
     plt.show()
 
 if __name__ == "__main__":
